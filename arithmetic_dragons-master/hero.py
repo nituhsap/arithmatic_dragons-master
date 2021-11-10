d9543511f27b573fc9c2cf960f40664034e9886d
nituhsap <nituhsap@mail.ru>
2021-11-10 21:09:43 +0300

    trying to make it work

--- arithmetic_dragons-master/hero.py
+++ arithmetic_dragons-master/hero.py
@@ -3,10 +3,12 @@
 from gameunit import *
 
 class Hero(Attacker):
-	def __init__(self):
-		self._health = 100
-		self._attack = 50
-		self._exp = 0
+    _experience = None
+    def __init__(self, name):
+        self._health = 100
+        self._attack = 50
+        self._experience = 0
+        self._name = name
 		
 
 #FIXME:
